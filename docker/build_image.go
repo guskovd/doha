7219f9bf0f98c3017886f8696578cb99ef53c178
Danil Guskov <d.guskov@corp.mail.ru>
2018-08-31 20:50:41 +0300

    artifacts cleanup

--- docker/build_image.go
+++ docker/build_image.go
@@ -8,10 +8,6 @@ import (
 	"os/user"
 	"os/exec"
 
-	// "github.com/jhoonb/archivex"
-	// "github.com/docker/docker/api/types"
-	// "github.com/docker/docker/client"
-	// "golang.org/x/net/context"
 )
 
 var dockerFileContent = []byte(`
@@ -64,48 +60,5 @@ func BuildImage() {
 		log.Fatal(string(out))
 		log.Fatal(lookErr)
 	}
-		
-
-	// tarfile, err := ioutil.TempFile("", "dohaDockerfileTar")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// tar := new(archivex.TarFile)
-	// // defer os.Remove(tarfile.Name())
-
-	// tar.Create(tarfile.Name())
-	// tar.AddAll(tmpdir, false)
-
-	// ctx := context.Background()
-	// dockerBuildContext, err := os.Open(tarfile.Name())
-
-	// cli, err := client.NewEnvClient()
-
-	// current_user, err := user.Current()
-	// current_group, err := user.LookupGroupId(current_user.Gid)
-
-	// log.Println(current_user.Username)
-	// log.Println(current_user.Uid)
-	// log.Println(current_user.Gid)
-	// log.Println(current_group.Name)
-
-	// buildOptions := types.ImageBuildOptions{
-	// 	Tags: []string{DohaImageLocal},
-	// 	SuppressOutput: true, // need!
-	// 	PullParent: true,
-	// 	BuildArgs: map[string]*string{
-	// 		"username": &current_user.Username,
-	// 		"userid": &current_user.Uid,
-	// 		// "groupid": &current_user.Gid,
-	// 		"groupname": &current_group.Name,
-	// 	},
-	// }
-
-	// _, err1 := cli.ImageBuild(ctx, dockerBuildContext, buildOptions)
-
-	// if err1 != nil {
-	// 	log.Fatal(err1)
-	// }
 }
 
